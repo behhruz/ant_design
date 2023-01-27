279272af697ed5477ec072077ac58ad481ad4a29
behhruz <bsatimbayev05@gmail.com>
2023-01-27 19:10:59 +0500

    Project started

--- src/Components/Design/index.jsx
+++ src/Components/Design/index.jsx
@@ -2,31 +2,40 @@ import React from "react";
 import { NavLink, Outlet, Route, Routes } from "react-router-dom";
 import { Navbars } from "../../Utilities/navbar";
 import { DesignBody } from "./DesignBody";
+import { Box, Container } from "./style";
 
 export const Design = () => {
   let design = Navbars.filter((v) => v.path == "design")[0].child;
 
   return (
     <>
-      <>
-        {design.map((v) => (
-          <NavLink  to={v.path}>
-            <h1>{v.title}</h1>
-          </NavLink>
-        ))}
-      </>
-      {Navbars.map((v) => {
-        return (
-          <>
-            {v.child?.map((i) => (
-              <Routes>
-                <Route path={i.path} element={<DesignBody title={i.title} />} />
-              </Routes>
+      <Box>
+        <>
+          <Container>
+            {" "}
+            {design.map((v) => (
+              <NavLink  style={{textDecoration: 'none'}} to={v.path}>
+                <h1>{v.title}</h1>
+              </NavLink>
             ))}
-          </>
-        );
-      })}
-      <Outlet />
+          </Container>
+        </>
+        {Navbars.map((v) => {
+          return (
+            <>
+              {v.child?.map((i) => (
+                <Routes>
+                  <Route
+                    path={i.path}
+                    element={<DesignBody title={i.title} />}
+                  />
+                </Routes>
+              ))}
+            </>
+          );
+        })}
+        <Outlet />
+      </Box>
     </>
   );
 };
---
+++ src/Components/Design/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 20%;
+  /* background-color: green; */
+  height: 84vh;
+  overflow-y: scroll;
+  scrollbar-color: rebeccapurple green;
+  scrollbar-width: thin;
+  border: ;
+`;
+const Box = styled.div`
+  display: flex;
+  margin: 35px 0 0 0;
+`;
+export { Container,Box };
